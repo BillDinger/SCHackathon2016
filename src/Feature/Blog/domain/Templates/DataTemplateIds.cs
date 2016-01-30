9bf9fa2257104ce8893c17f3968927775cc45821
tyesser <ty.esser@vml.com>
2016-01-30 08:20:47 -0600

    template ids for containers

--- src/Feature/Blog/domain/Templates/DataTemplateIds.cs
+++ src/Feature/Blog/domain/Templates/DataTemplateIds.cs
@@ -10,5 +10,7 @@ namespace Sitecore.Feature.Blog.Domain.Templates
         public const string BlogAuthor = "{A767A2DD-4742-4D85-BB8D-27853C6143A3}";
         public const string BlogCategory = "{A0D2FD2C-3D32-4CC8-8EA2-56E756F0F0F8}";
         public const string BlogRenderingParameters = "{CBFEE2CE-DF46-48DB-A8D6-0F094D2972C0}";
+        public const string BlogCategoriesFolder = "{543D09EF-903C-43EB-A548-4D6CC263F57F}";
+        public const string BlogPosts = "{EF6ACDEF-E620-4293-8691-02435754FE55}";
     }
 }
