9450798e5b2e1f6972b3354e23254baa1cbf74b0
Bill Dinger <bill.dinger@vml.com>
2016-01-30 02:13:08 -0600

    glass config and contexts

--- src/Feature/Blog/Sitecore.Feature.Blog/App_Start/GlassMapperScCustom.cs
+++ src/Feature/Blog/Sitecore.Feature.Blog/App_Start/GlassMapperScCustom.cs
@@ -6,29 +6,35 @@ using IDependencyResolver = Glass.Mapper.Sc.IoC.IDependencyResolver;
 
 namespace Sitecore.Feature.Blog.App_Start
 {
-    public static  class GlassMapperScCustom
+    using Glass.Mapper.Sc.Configuration.Attributes;
+
+    public static class GlassMapperScCustom
     {
-		public static IDependencyResolver CreateResolver(){
-			var config = new Glass.Mapper.Sc.Config();
+        public static IDependencyResolver CreateResolver()
+        {
+            var config = new Glass.Mapper.Sc.Config();
 
-			var dependencyResolver = new DependencyResolver(config);
-			// add any changes to the standard resolver here
-			return dependencyResolver;
-		}
+            var dependencyResolver = new DependencyResolver(config);
+            // add any changes to the standard resolver here
+            return dependencyResolver;
+        }
 
-		public static IConfigurationLoader[] GlassLoaders(){			
-			
-			/* USE THIS AREA TO ADD FLUENT CONFIGURATION LOADERS
+        public static IConfigurationLoader[] GlassLoaders()
+        {
+
+            /* USE THIS AREA TO ADD FLUENT CONFIGURATION LOADERS
              * 
              * If you are using Attribute Configuration or automapping/on-demand mapping you don't need to do anything!
              * 
              */
+            var _domainAttributes = new SitecoreAttributeConfigurationLoader("Sitecore.Feature.Blog.Domain");
 
-			return new IConfigurationLoader[]{};
-		}
-		public static void PostLoad(){
-			//Remove the comments to activate CodeFist
-			/* CODE FIRST START
+            return new IConfigurationLoader[] { _domainAttributes };
+        }
+        public static void PostLoad()
+        {
+            //Remove the comments to activate CodeFist
+            /* CODE FIRST START
             var dbs = Sitecore.Configuration.Factory.GetDatabases();
             foreach (var db in dbs)
             {
@@ -43,10 +49,10 @@ namespace Sitecore.Feature.Blog.App_Start
             }
              * CODE FIRST END
              */
-		}
-		public static void AddMaps(IConfigFactory<IGlassMap> mapsConfigFactory)
+        }
+        public static void AddMaps(IConfigFactory<IGlassMap> mapsConfigFactory)
         {
-			// Add maps here
+            // Add maps here
             // mapsConfigFactory.Add(() => new SeoMap());
         }
     }
