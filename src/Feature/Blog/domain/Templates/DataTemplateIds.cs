601a140f54a6d1b97af5001bb0c07d85e1c6385c
tyesser <ty.esser@vml.com>
2016-01-30 06:51:47 -0600

    added rendering param domain item and modified blog detail to be bucketable

--- src/Feature/Blog/domain/Templates/DataTemplateIds.cs
+++ src/Feature/Blog/domain/Templates/DataTemplateIds.cs
@@ -1,11 +1,14 @@
 ﻿
 namespace Sitecore.Feature.Blog.Domain.Templates
 {
+    using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
+
     public static class DataTemplateIds
     {
         public const string BlogListing = "{DB981D66-7BA8-48BF-9917-753827A8321F}";
         public const string BlogDetail = "{40C7A406-D806-47F8-B017-94605781FB0F}";
         public const string BlogAuthor = "{A767A2DD-4742-4D85-BB8D-27853C6143A3}";
         public const string BlogCategory = "{A0D2FD2C-3D32-4CC8-8EA2-56E756F0F0F8}";
+        public const string BlogRenderingParameters = "{CBFEE2CE-DF46-48DB-A8D6-0F094D2972C0}";
     }
 }
