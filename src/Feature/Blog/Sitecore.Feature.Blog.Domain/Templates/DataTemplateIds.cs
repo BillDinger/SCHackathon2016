1c62b40f755a3232ce1a6fe14cbea29d0bca0d2e
tyesser <ty.esser@vml.com>
2016-01-30 03:03:11 -0600

    TemplateIds

---
+++ src/Feature/Blog/Sitecore.Feature.Blog.Domain/Templates/DataTemplateIds.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Sitecore.Feature.Blog.Domain.Templates
+{
+    public static class DataTemplateIds
+    {
+        public const string BlogListing = "{DB981D66-7BA8-48BF-9917-753827A8321F}";
+        public const string BlogDetail = "{40C7A406-D806-47F8-B017-94605781FB0F}";
+        public const string BlogAuthor = "{A767A2DD-4742-4D85-BB8D-27853C6143A3}";
+        public const string BlogCategory = "{A0D2FD2C-3D32-4CC8-8EA2-56E756F0F0F8}";
+    }
+}
