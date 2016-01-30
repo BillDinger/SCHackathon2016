1a21d40345d00dc058f5e6e154a6342b6befb088
Bill Dinger <bill.dinger@vml.com>
2016-01-30 01:33:26 -0600

    added the windsor isntallers

---
+++ src/Feature/Blog/Sitecore.Feature.Blog.CMS/Installers/CMSWindsorInstaller.cs
@@ -0,0 +1,13 @@
+﻿namespace Sitecore.Feature.Blog.CMS.Installers
+{
+    using Castle.MicroKernel.Registration;
+    using Castle.MicroKernel.SubSystems.Configuration;
+    using Castle.Windsor;
+
+    public class CMSWindsorInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+        }
+    }
+}
---
+++ src/Feature/Blog/Sitecore.Feature.Blog/Installers/BlogWindsorInstaller.cs
@@ -0,0 +1,13 @@
+﻿namespace Sitecore.Feature.Blog.Installers
+{
+    using Castle.MicroKernel.Registration;
+    using Castle.MicroKernel.SubSystems.Configuration;
+    using Castle.Windsor;
+
+    public class BlogWindsorInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+        }
+    }
+}
