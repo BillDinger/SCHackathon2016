0d6f815743a24888fe98b4acb8f14089f81c5642
Bill Dinger <bill.dinger@vml.com>
2016-01-30 02:45:04 -0600

    Added moq and fake db

---
+++ src/Feature/Blog/Sitecore.Feature.Blog.CMS/Factories/DefaultCmsFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace Sitecore.Feature.Blog.CMS.Factories
+{
+    public class DefaultCmsFactory : ICmsFactory
+    {
+        public T Create<T>() where T : class
+        {
+            return null;
+        }
+    }
+}
---
+++ src/Feature/Blog/Sitecore.Feature.Blog.CMS/Factories/ICmsFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace Sitecore.Feature.Blog.CMS.Factories
+{
+    public interface ICmsFactory
+    {
+        T Create<T>() where T : class;
+    }
+}
