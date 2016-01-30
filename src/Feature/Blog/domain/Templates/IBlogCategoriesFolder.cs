3a1ea75ac22abcb7fd2829dad53a8b9e44054f07
tyesser <ty.esser@vml.com>
2016-01-30 08:23:26 -0600

    Added SitecoreType mapping to container domain items

--- src/Feature/Blog/domain/Templates/IBlogCategoriesFolder.cs
+++ src/Feature/Blog/domain/Templates/IBlogCategoriesFolder.cs
@@ -1,6 +1,8 @@
-﻿
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+
 namespace Sitecore.Feature.Blog.Domain.Templates
 {
+    [SitecoreType(TemplateId = DataTemplateIds.BlogCategoriesFolder, AutoMap = true)]
     public interface IBlogCategoriesFolder
     {
     }
--- src/Feature/Blog/domain/Templates/IBlogPosts.cs
+++ src/Feature/Blog/domain/Templates/IBlogPosts.cs
@@ -1,5 +1,8 @@
-﻿namespace Sitecore.Feature.Blog.Domain.Templates
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+
+namespace Sitecore.Feature.Blog.Domain.Templates
 {
+    [SitecoreType(TemplateId = DataTemplateIds.BlogPosts, AutoMap = true)]
     public interface IBlogPosts
     {
     }
