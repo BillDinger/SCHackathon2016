d3fa4599d662c812dbd8b304bb28a1bcdc71bda2
Bill Dinger <bill.dinger@vml.com>
2016-01-30 01:53:22 -0600

    logger implementation and tests

--- src/Feature/Blog/Sitecore.Feature.Blog.CMS/Log/DefaultLogger.cs
+++ src/Feature/Blog/Sitecore.Feature.Blog.CMS/Log/DefaultLogger.cs
@@ -1,7 +1,51 @@
 ﻿namespace Sitecore.Feature.Blog.CMS.Log
 {
-    public class DefaultLogger
+    using System;
+
+    public class DefaultLogger : ILogger
     {
-         
+        private const string LogPrepend = "[BLOG]";
+
+        public void Audit(string message, object owner)
+        {
+            var formattedMessage = $"{LogPrepend} {message}";
+            Diagnostics.Log.Audit(formattedMessage, owner);
+        }
+
+        public void Debug(string message, object owner)
+        {
+            var formattedMessage = $"{LogPrepend} {message}";
+            Diagnostics.Log.Debug(formattedMessage, owner);
+            Diagnostics.Tracer.Debug(formattedMessage);
+        }
+
+        public void Error(string message, Exception ex, object owner)
+        {
+            var formattedMessage = $"{LogPrepend} {message}";
+            Diagnostics.Log.Error(formattedMessage, ex, owner);
+            Diagnostics.Tracer.Error(formattedMessage);
+        }
+
+        public void Fatal(string message, object owner)
+        {
+            var formattedMessage = $"{LogPrepend} {message}";
+            Diagnostics.Log.Fatal(formattedMessage, owner);
+            Diagnostics.Tracer.Fatal(formattedMessage);
+        }
+
+        public void Info(string message, object owner)
+        {
+            var formattedMessage = $"{LogPrepend} {message}";
+
+            Diagnostics.Log.Info(formattedMessage, owner);
+            Diagnostics.Tracer.Info(formattedMessage, owner);
+        }
+
+        public void Warn(string message, object owner)
+        {
+            var formattedMessage = $"{LogPrepend} {message}";
+            Diagnostics.Log.Warn(formattedMessage, owner);
+            Diagnostics.Tracer.Warning(formattedMessage, owner);
+        }
     }
 }
